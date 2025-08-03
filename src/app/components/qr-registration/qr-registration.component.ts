22a7945dd4571df76de2a16fbbed0434f4c13725
Rahul Verma <rahul.k.verma@forushealth.com>
2025-08-03 17:13:19 +0530

    qr reg form new ui

--- src/app/components/qr-registration/qr-registration.component.ts
+++ src/app/components/qr-registration/qr-registration.component.ts
@@ -1,7 +1,7 @@
-import { CUSTOM_ELEMENTS_SCHEMA, ChangeDetectionStrategy, Component, Inject, LOCALE_ID, OnInit,inject, model } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, LOCALE_ID, OnInit,inject, model } from '@angular/core';
 import { SharedServiceService } from '../../app-services/shared-service/shared-service.service';
 
-import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule, FormGroup} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatStepperModule} from '@angular/material/stepper';
@@ -15,12 +15,13 @@ import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 import { ClockComponent } from '../child-components/clock/clock.component';
 import { Time } from '../../model/Time';
 import { EventData } from '../../model/EventData';
-import { formatDate } from '@angular/common';
+import { CommonModule, formatDate } from '@angular/common';
 import { RegisterWebinarFormFields } from '../../model/RegisterWebinarFormFields';
 import { ActivatedRoute } from '@angular/router';
 import { ApiServiceService } from '../../app-services/api-service/api-service.service';
 import { ApiResponse } from '../../model/ApiResponse';
 import { Payment } from '../../model/SuccessPayment';
+import { LoaderComponent } from '../loader/loader.component';
 
 declare var $:any;
 declare var Razorpay:any;
@@ -40,7 +41,9 @@ declare var Razorpay:any;
     MatCardModule, 
     MatDatepickerModule,
     NgxMaterialTimepickerModule,
-    ClockComponent
+    CommonModule,
+    ClockComponent,
+    LoaderComponent
   ],
   //changeDetection: ChangeDetectionStrategy.OnPush,
   providers:[MatNativeDateModule],
@@ -50,6 +53,8 @@ export class QrRegistrationComponent implements OnInit{
   private _formBuilder = inject(FormBuilder);
   firstFormGroup:any;
   secondFormGroup:any;
+  qrRegistrationForm:FormGroup;
+  qrRegistrationFormSubmitted:boolean = false;
   isLinear:boolean= false;
   calenderDate = model<Date | null>(null);
   selectedDate:string="";
@@ -61,19 +66,36 @@ export class QrRegistrationComponent implements OnInit{
   userData:RegisterWebinarFormFields = new RegisterWebinarFormFields();
   qrData:any = {};
   showPanel:string = 'HOME';
+  showLoader:boolean = false;
+  showPaymentCompletedMessage:boolean = false;
 
   //http://localhost:4200/qr-registration?data={"courseId":"aa529bb9-9fac-44eb-a20d-849e2b8137d7","courseTitle":"Online Webinar","courseFee":"10","courseDurationInHrs":"20"}
   constructor(@Inject(LOCALE_ID) public locale:string,
               private sharedService:SharedServiceService,
               private apiService:ApiServiceService,
-              private activateRoute:ActivatedRoute){
+              private activateRoute:ActivatedRoute,
+              private formBuilder:FormBuilder,
+              private cdr:ChangeDetectorRef){
     this.calenderDate.subscribe((data:any)=>{
       this.selectedDate = formatDate(data,'dd MMMM YYYY',this.locale);
-    })
+    });
+
+    this.qrRegistrationForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      middleName: [''],
+      lastName: [''],
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', [Validators.required, Validators.min(10)]],
+      location: [''],
+      occupation: [''],
+    });
 
   }
 
   ngOnInit(): void {
+    this.qrRegistrationFormSubmitted = false;
+    this.showLoader = false;
+    this.showPaymentCompletedMessage = false;
     this.showPanel = 'HOME';
 
     $("#global-header").css("display","none");
@@ -168,14 +190,27 @@ export class QrRegistrationComponent implements OnInit{
   }
 
   initiatePayment(){
+    this.qrRegistrationFormSubmitted = true;
+    if (!this.qrRegistrationForm.valid) {
+      console.log("manadatory fields are missing");
+    }
+    else{
     //public :rzp_test_OuzWZwerLWicgC	
     //secret : 6L5XmmY7Q1q3kqPN4s0tmf6P
 
+    this.userData.firstName = this.qrRegistrationForm.value.firstName;
+    this.userData.middleName = this.qrRegistrationForm.value.middleName;
+    this.userData.lastName = this.qrRegistrationForm.value.lastName;
+    this.userData.email = this.qrRegistrationForm.value.email;
+    this.userData.mobile = this.qrRegistrationForm.value.mobile;
+    this.userData.location = this.qrRegistrationForm.value.location;
+    this.userData.occupation = this.qrRegistrationForm.value.occupation;
     this.userData.initiatePayment.amount = this.qrData.courseFee;
     this.userData.initiatePayment.currency = "INR";
     this.userData.courseId = Number(this.qrData.courseId);
     this.userData.source = "QR_WEBAPP";
     
+    this.showLoader = true;
     this.apiService.registerWebinarUser(this.userData).subscribe({
       next: (response:ApiResponse) => {
         console.log('initiate payment successfull', response);
@@ -230,9 +265,11 @@ export class QrRegistrationComponent implements OnInit{
         console.error('error in initiate payment', error);
       },
       complete: () => {
+        this.showLoader = false;
         console.log('initiate payment completed');
       }
     });
+    }
   }
 
   onPaymentSuccess(obj:any){
@@ -242,17 +279,22 @@ export class QrRegistrationComponent implements OnInit{
     payment.razorpaySignature = obj.razorpay_signature;
     payment.source = "QR_WEBAPP";
 
+    this.showLoader = true;
+    this.cdr.detectChanges();
     this.apiService.onPaymentSuccess(payment).subscribe({
       next: (response:ApiResponse) => {
         console.log('initiate payment successfull', response);
         if(response.responseCode == 200){
           console.log("PAYMENT SUCCESSFULL");
+          this.showPaymentCompletedMessage = true;
+          this.cdr.detectChanges();
         }
       },
       error: (error) => {
         console.error('error in initiate payment', error);
       },
       complete: () => {
+        this.showLoader = false;
         console.log('initiate payment completed');
       }
     });
